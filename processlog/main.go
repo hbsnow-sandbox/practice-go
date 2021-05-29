60be5a3f406e7df47d9f4a7f6ff58c05da0df241
hbsnow <yuki.4uing@gmail.com>
2021-05-29 14:31:25 +0900

    feat: processlog

---
+++ processlog/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	logDir, commandName, args := parseArgs()
+	stdout, stderr, err := initOut(logDir, commandName)
+	if err != nil {
+		fmt.Fprintf(stderr, "Can't create file: %v\n", err)
+		os.Exit(1)
+	}
+
+	startTime := time.Now()
+	ps, err := execution(commandName, args, stdout, stderr)
+	if err != nil {
+		fmt.Fprintf(stderr, "Can't exec command: %v\n", err)
+		os.Exit(1)
+	}
+	exitTime := time.Now()
+	fmt.Fprintln(stdout, ps.String())
+	fmt.Fprintf(
+		stdout,
+		"walll clock time=%v system time=%v user time=%v\n",
+		exitTime.Sub(startTime),
+		ps.SystemTime(),
+		ps.UserTime(),
+	)
+}
